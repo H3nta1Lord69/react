89211c6950c0f1a09a00d26ea882ae91003e433c
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-09-11 23:13:01 -0500

    chore: variables

---
+++ 02-js/src/01-const-let.js
@@ -0,0 +1,15 @@
+const name = "Carl";
+const lastName = "Rivero";
+
+let givenValue = 4;
+givenValue = 6;
+
+console.log(name, lastName, givenValue);
+
+if (true) {
+  let givenValue = 10;
+
+  console.log(givenValue);
+}
+
+console.log(givenValue);
