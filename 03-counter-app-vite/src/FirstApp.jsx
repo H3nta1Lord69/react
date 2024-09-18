683069cb7cc56b0eb481f547d28a3159bc5b9416
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-09-17 23:03:08 -0500

    chore: fragment

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,3 +1,4 @@
+// import { Fragment } from "react";
 // function App() {
 //   // document.createElement...
 //   return <h1>Hello world</h1>;
@@ -7,8 +8,12 @@
 
 // Functional component
 export const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>A paragraph</p>
+    </>
+  );
 };
-
 
 // export const anotherApp = () => <h1>Arrow function</h1>
