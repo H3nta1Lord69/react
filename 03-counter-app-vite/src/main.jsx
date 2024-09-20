edb6bdbe33509b1ac398579d0cc567f92975d005
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-09-19 21:58:01 -0500

    chore: styles

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./FirstApp";
 import { AnotherApp } from "./app.component";
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
