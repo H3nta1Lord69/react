a7ebd058fac32a857ffe7523c29df74df60711a7
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-09-23 23:13:10 -0500

    chore: propTypes

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,11 +1,23 @@
+import PropTypes from "prop-types";
+
 export const FirstApp = ({ title, subtitle }) => {
   // console.lo g(props);
+
+  // if (!title) {
+  //   throw new Error("Title doesn't exists");
+  // }
 
   return (
     <>
       <h1>{title}</h1>
       {/* <code>{JSON.stringify(newMessageAnything)}</code> */}
-      <p>{subtitle + 1}</p>
+      <p>{subtitle}</p>
     </>
   );
+};
+
+FirstApp.propTypes = {
+  // Mandatory prop type
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.number,
 };
