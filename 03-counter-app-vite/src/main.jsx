91c2d24448a8cb257553f0adb6b07d24fc8798e5
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-09-24 22:46:20 -0500

    chore: counterApp

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FirstApp } from "./FirstApp";
+// import { FirstApp } from "./FirstApp";
 // import { AnotherApp } from "./app.component";
 import './styles/index.css'
+import { CounterApp } from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp title="Hola, soy Goku!" subTitle={123456} />
+    {/* <FirstApp title="Hola, soy Goku!" subTitle={123456} /> */}
     {/* <AnotherApp /> */}
+    <CounterApp value={123}/>
   </React.StrictMode>
 );
