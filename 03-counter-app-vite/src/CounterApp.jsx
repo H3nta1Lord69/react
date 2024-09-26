b19487d5fb116f582f77cde4e57f88b25da61ea0
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-09-25 19:36:58 -0500

    chore: usestate

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
   const handleAdd = (event) => {
+    // setCounter((c) => c + 1);
+    setCounter(counter + 1);
+
     console.log(event);
-  }
+  };
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
     </>
   );
