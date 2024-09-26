864e94f307556385b93ea867f70e249c5ee494a0
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-09-25 21:39:31 -0500

    chore: expect to be

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,5 +1,13 @@
-test("This is a demo test", () => {
-  if (1 === 0) {
-    throw new Error("It can't be divided");
-  }
+describe("Tests on <DemoComponent />", () => {
+  test("This is a demo test", () => {
+    // AAA
+    // 1. Initial state
+    const msg1 = `Hello world`;
+
+    // 2. Stimulation
+    const msg2 = msg1.trim();
+
+    // 3. Asserts
+    expect(msg1).toBe(msg2);
+  });
 });
