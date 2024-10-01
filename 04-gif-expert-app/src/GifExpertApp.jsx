be3981061958a886d1396da119f9db88504f50e7
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-09-30 23:08:13 -0500

    chore: styles and TODOs

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,7 +3,12 @@ import React from "react";
 export const GifExpertApp = () => {
   return (
     <>
+      {/* Title */}
       <h1>GifExpertApp</h1>
+      {/* Input */}
+
+      {/* Gif list */}
+      {/* gifItem */}
     </>
   );
 };
