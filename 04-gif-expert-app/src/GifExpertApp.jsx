ddeef6b30ae092e19a1bcabe414755b5832fbea5
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-10-01 21:57:45 -0500

    chore: add category

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -2,8 +2,14 @@ import React, { useState } from "react";
 
 export const GifExpertApp = () => {
   // Isn't recommendable to condition a hook
+  // Avoid mutations
 
   const [categories, setCategories] = useState(["One Piece", "Pokémon"]);
+
+  const onAddCategory = () => {
+    // setCategories(cat => [...categories, "Jojo's"]);
+    setCategories(["Jojo's", ...categories]);
+  };
 
   return (
     <>
@@ -12,6 +18,7 @@ export const GifExpertApp = () => {
       {/* Input */}
 
       {/* Gif list */}
+      <button onClick={onAddCategory}>Add Category</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
