be201352b58177c23d166350995b44da7014efb2
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-10-16 21:23:26 -0500

    chore: gifgrid fetch giphy api

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -6,7 +6,7 @@ export const GifExpertApp = () => {
   // Isn't recommendable to condition a hook
   // Avoid mutations
 
-  const [categories, setCategories] = useState(["One Piece", "Pokémon"]);
+  const [categories, setCategories] = useState(["One Piece"]);
 
   const onAddCategory = (newCategory) => {
     if (categories.includes(newCategory)) return;
--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,6 +1,22 @@
 import PropTypes from "prop-types";
 
 export const GifGrid = ({ category }) => {
+  const getGifs = async () => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=0l5E6D2KZMCQhT3IJoSuGG2o8ARiNHyf&q=${category}&limit=20`;
+    const resp = await fetch(url);
+    const { data = [] } = await resp.json();
+
+    const gifs = data.map((img) => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    }));
+
+    console.log(gifs);
+  };
+
+  getGifs();
+
   return (
     <>
       <h3>{category}</h3>
