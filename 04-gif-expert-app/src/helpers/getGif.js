f22db879f83a576654222e4774a0f14ba025b709
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-10-16 21:27:13 -0500

    chore: optimize function

---
+++ 04-gif-expert-app/src/helpers/getGif.js
@@ -0,0 +1,14 @@
+export const getGif = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=0l5E6D2KZMCQhT3IJoSuGG2o8ARiNHyf&q=${category}&limit=20`;
+  const resp = await fetch(url);
+  const { data = [] } = await resp.json();
+
+  const gif = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+
+  console.log(gif);
+  return gif;
+};
