e7011b3759d5e6c51d4bf9bc771753700b1a8dad
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-10-17 23:18:44 -0500

    fix: rendering gifs

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,17 +1,32 @@
 import PropTypes from "prop-types";
 import { getGif } from "../helpers/getGif";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImage = await getGif(category);
+    setImages(newImage);
+  };
+
   // Trigger an effect in any desired condition
   useEffect(() => {
-    getGif(category);
+    getImages();
     // [] means it will trigger a single time
   }, []);
 
   return (
     <>
       <h3>{category}</h3>
+
+      <ol>
+        {images.map(({ id, title, url }) => (
+          <li key={id} alt={title}>
+            <img src={url}></img>
+          </li>
+        ))}
+      </ol>
     </>
   );
 };
--- 04-gif-expert-app/src/helpers/getGif.js
+++ 04-gif-expert-app/src/helpers/getGif.js
@@ -1,5 +1,5 @@
 export const getGif = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=0l5E6D2KZMCQhT3IJoSuGG2o8ARiNHyf&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=0l5E6D2KZMCQhT3IJoSuGG2o8ARiNHyf&q=${category}&limit=5`;
   const resp = await fetch(url);
   const { data = [] } = await resp.json();
 
