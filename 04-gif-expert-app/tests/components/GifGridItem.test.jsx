7beb95a6c2133b9424d92df38a689f82ce5c0aff
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-10-28 22:19:50 -0500

    chore: url and alt props test

--- 04-gif-expert-app/tests/components/GifGridItem.test.jsx
+++ 04-gif-expert-app/tests/components/GifGridItem.test.jsx
@@ -5,7 +5,18 @@ describe("Tests on GifGridItem", () => {
   const title = "One Piece";
   const url = "https://www.google.com";
   test("should match with snapshot", () => {
-    const {container} = render(<GifGridItem title={title} url={url} />);
-    expect(container).toMatchSnapshot(); 
+    const { container } = render(<GifGridItem title={title} url={url} />);
+    expect(container).toMatchSnapshot();
   });
+});
+
+test("should present the image with the URL and ALT", () => {
+  render(<GifGridItem title={title} url={url} />);
+  // screen.debug();
+  // expect(screen.getByRole("img").src).toBe(url);
+  // expect(screen.getByRole("img").alt).toBe(title);
+
+  const { src, alt } = screen.getByRole("img");
+  expect(src).toBe(url);
+  expect(alt).toBe(title);
 });
