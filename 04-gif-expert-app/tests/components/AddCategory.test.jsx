59ea59e7ecb342a56632398fc6ab533880fea46e
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-11-14 21:38:50 -0500

    chore: addcategory tests

--- 04-gif-expert-app/tests/components/AddCategory.test.jsx
+++ 04-gif-expert-app/tests/components/AddCategory.test.jsx
@@ -12,9 +12,10 @@ describe("AddCategory tests", () => {
 
   test("should call onNewCategory if the input have a value", () => {
     const inputValue = "Luffy";
+    const onNewCategory = jest.fn();
     //TODO:
 
-    render(<AddCategory onNewCategory={() => {}} />);
+    render(<AddCategory onNewCategory={onNewCategory} />);
 
     const input = screen.getByRole("textbox");
     const form = screen.getByRole("form");
@@ -23,5 +24,22 @@ describe("AddCategory tests", () => {
     fireEvent.submit(form);
     // screen.debug();
     expect(input.value).toBe("");
+
+    expect(onNewCategory).toHaveBeenCalled();
+    // Call only one time the function
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    // Validate value of the function
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+
+  test(`shouldn't call the function if the input is empty`, () => {
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
+    const form = screen.getByRole("form");
+
+    fireEvent.submit(form);
+
+    expect(onNewCategory).toHaveBeenCalledTimes(0);
+    expect(onNewCategory).not.toHaveBeenCalledTimes(1);
   });
 });
