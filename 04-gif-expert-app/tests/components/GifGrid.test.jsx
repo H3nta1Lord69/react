d500150fad7c0cbcb35f772b38f5a1703ad6baf9
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2024-11-24 18:43:27 -0500

    chore: gif grid test

--- 04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,12 +1,44 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGif } from "../../src/hooks/useFetchGif";
+
+jest.mock("../../src/hooks/useFetchGif");
 
 describe("Tests on <GidGrid />", () => {
   const category = "One Punch";
 
   test("should shown the loading", () => {
+    useFetchGif.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
     render(<GifGrid category={category} />);
     expect(screen.getByText("Loading..."));
     expect(screen.getByText(category));
+  });
+
+  test(`should show items when they're loaded`, () => {
+    const gif = [
+      {
+        id: "ABC",
+        title: "Dragon",
+        url: "https://localhost/dragon.jpg",
+      },
+      {
+        id: "123",
+        title: "One Piece",
+        url: "https://localhost/one-piece.jpg",
+      },
+    ];
+
+    useFetchGif.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
+    render(<GifGrid category={category} />);
+
+    expect(screen.getAllByRole("img").length).toBe(2);
   });
 });
